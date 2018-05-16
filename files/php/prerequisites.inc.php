14f671359b37675bf54cda27bafd5bc1a4282fce
Phoenix Eve Aspacio <aspaciop@phoenixpeca.xyz>
2018-05-16 20:09:57 +0800

    Update prerequisites.inc.php

--- files/php/prerequisites.inc.php
+++ files/php/prerequisites.inc.php
@@ -1,1 +1,6 @@
-<?php /* Leaving this without php tag outputs line characters to response */ ?>
+<?php
+/***
+You may add custom behaviors here.
+You can check out `misc.inc.php` for an example of what situation this
+file might be useful.
+***/ 
